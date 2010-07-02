32bc374aaa45d0e098016f8e0c73d30c76a37629
dhdfu <dhdfu@94700074-3cef-4d97-a70e-9c8c206c02f5>
2010-07-02 18:16:41 +0000

    subclass button for hold-to-talk (not done yet) and also make a decoder thread
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/PocketSphinxAndroidDemo@10190 94700074-3cef-4d97-a70e-9c8c206c02f5

--- src/edu/cmu/pocketsphinx/demo/PocketSphinxDemo.java
+++ src/edu/cmu/pocketsphinx/demo/PocketSphinxDemo.java
@@ -1,30 +1,62 @@
 package edu.cmu.pocketsphinx.demo;
 
 import android.app.Activity;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import edu.cmu.pocketsphinx.Config;
 import edu.cmu.pocketsphinx.Decoder;
 import edu.cmu.pocketsphinx.pocketsphinx;
 
-public class PocketSphinxDemo extends Activity {
+class RecognizerTask implements Runnable {
 	Decoder ps;
-	static {
-		System.loadLibrary("pocketsphinx_jni");
+	AudioRecord rec;
+	
+	public RecognizerTask() {
+		this.createDecoder();
+		this.createAudio();
 	}
-	public void createDecoder() {
+
+	void createDecoder() {
+        pocketsphinx.setLogfile("/sdcard/Android/data/edu.cmu.pocketsphinx/pocketsphinx.log");
 		Config c = new Config();
 		/* In 2.2 and above we can use getExternalFilesDir() or whatever it's called */
 		c.setString("-hmm", "/sdcard/Android/data/edu.cmu.pocketsphinx/hmm/en_US/hub4wsj_sc_8k");
 		c.setString("-dict", "/sdcard/Android/data/edu.cmu.pocketsphinx/lm/en_US/hub4.5000.dic");
 		c.setString("-lm", "/sdcard/Android/data/edu.cmu.pocketsphinx/lm/en_US/hub4.5000.DMP");
+		/* Necessary because binary data is always big-endian in Java. */
+		c.setString("-input_endian", "big");
 		this.ps = new Decoder(c);
 	}
+	
+	void createAudio() {
+		this.rec = new AudioRecord(MediaRecorder.AudioSource.DEFAULT,
+					8000, AudioFormat.CHANNEL_IN_MONO,AudioFormat.ENCODING_PCM_16BIT,
+					8192);
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+}
+
+public class PocketSphinxDemo extends Activity {
+	static {
+		System.loadLibrary("pocketsphinx_jni");
+	}
+	RecognizerTask rec;
+	Thread rec_thread;
+	
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        pocketsphinx.setLogfile("/sdcard/Android/data/edu.cmu.pocketsphinx/pocketsphinx.log");
-        createDecoder();
         setContentView(R.layout.main);
+        this.rec = new RecognizerTask();
+        this.rec_thread = new Thread(this.rec);
+        HoldButton b = (HoldButton) findViewById(R.id.Button01);
     }
 }
