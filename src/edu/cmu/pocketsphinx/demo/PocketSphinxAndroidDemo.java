97ab02699a180264288c3c2fb87ff23189738265
Alexander Solovets <asolovets@gmail.com>
2013-10-08 11:52:56 +0000

    Install model files during runtime
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/PocketSphinxAndroidDemo@11968 94700074-3cef-4d97-a70e-9c8c206c02f5

--- src/edu/cmu/pocketsphinx/demo/PocketSphinxAndroidDemo.java
+++ src/edu/cmu/pocketsphinx/demo/PocketSphinxAndroidDemo.java
@@ -1,5 +1,8 @@
 package edu.cmu.pocketsphinx.demo;
 
+import java.io.File;
+import java.io.IOException;
+
 import android.app.Activity;
 
 import android.media.AudioFormat;
@@ -18,6 +21,7 @@ import edu.cmu.pocketsphinx.Config;
 import edu.cmu.pocketsphinx.Decoder;
 import edu.cmu.pocketsphinx.Hypothesis;
 
+import static edu.cmu.pocketsphinx.SphinxUtil.syncAssets;
 import static edu.cmu.pocketsphinx.sphinxbase.setLogFile;
 
 
@@ -29,16 +33,30 @@ public class PocketSphinxAndroidDemo extends Activity {
         private final Decoder decoder;
 
         public RecognitionTask() {
+            File root = null;
+
+            try {
+                root = syncAssets(getApplicationContext(), "models");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            setLogFile(new File(root, "pocketsphinx.log").getPath());
+
             Config config = Decoder.defaultConfig();
-            config.setString("-hmm", DATA_PATH + "hmm/hub4wsj_sc_8k");
-            config.setString("-dict", DATA_PATH + "lm/hub4.5000.dic");
-            config.setString("-lm", DATA_PATH + "lm/hub4.5000.DMP");
-            config.setString("-rawlogdir", DATA_PATH);
+            config.setString("-lm",
+                    new File(root, "lm/hub4.5000.DMP").getPath());
+            config.setString("-hmm",
+                    new File(root, "hmm/hub4wsj_sc_8k").getPath());
+            config.setString("-dict",
+                    new File(root, "lm/hub4.5000.dic").getPath());
+            config.setString("-rawlogdir", root.getPath());
             config.setFloat("-samprate", SAMPLE_RATE);
             config.setInt("-maxhmmpf", 10000);
             config.setBoolean("-backtrace", true);
             config.setBoolean("-bestpath", false);
             config.setBoolean("-remove_noise", false);
+
             decoder = new Decoder(config);
         }
 
@@ -62,14 +80,10 @@ public class PocketSphinxAndroidDemo extends Activity {
         }
     }
 
-    private static final String DATA_PATH =
-        "/sdcard/Android/data/edu.cmu.pocketsphinx/";
-
     private static final int SAMPLE_RATE = 8000;
 
     static {
         System.loadLibrary("pocketsphinx_jni");
-        setLogFile(DATA_PATH + "pocketsphinx.log");
     }
 
     private TextView speechResult;
