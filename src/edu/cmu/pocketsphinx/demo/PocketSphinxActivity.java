d1d07f48b2113ca924c3598a02b52318e84497ab
Nickolay Shmyrev <nshmyrev@gmail.com>
2014-05-16 11:32:45 +0000

    Sync API for assets sync and async task for recognizer initialization for better UI experience
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/pocketsphinx-android-demo@12478 94700074-3cef-4d97-a70e-9c8c206c02f5

--- src/edu/cmu/pocketsphinx/demo/PocketSphinxActivity.java
+++ src/edu/cmu/pocketsphinx/demo/PocketSphinxActivity.java
@@ -1,46 +1,104 @@
+/* ====================================================================
+ * Copyright (c) 2014 Alpha Cephei Inc.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ALPHA CEPHEI INC. ``AS IS'' AND
+ * ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY
+ * NOR ITS EMPLOYEES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ====================================================================
+ */
+
 package edu.cmu.pocketsphinx.demo;
 
 import static android.widget.Toast.makeText;
 import static edu.cmu.pocketsphinx.SpeechRecognizerSetup.defaultSetup;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
-import java.util.Map;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
-import edu.cmu.pocketsphinx.*;
-
+import edu.cmu.pocketsphinx.Assets;
+import edu.cmu.pocketsphinx.Hypothesis;
+import edu.cmu.pocketsphinx.RecognitionListener;
+import edu.cmu.pocketsphinx.SpeechRecognizer;
 
 public class PocketSphinxActivity extends Activity implements
-        RecognitionListener, AssetsTaskCallback {
+        RecognitionListener {
 
-    private static final String KWS_SEARCH_NAME = "wakeup";
+    private static final String KWS_SEARCH = "wakeup";
     private static final String FORECAST_SEARCH = "forecast";
     private static final String DIGITS_SEARCH = "digits";
     private static final String MENU_SEARCH = "menu";
     private static final String KEYPHRASE = "oh mighty computer";
 
     private SpeechRecognizer recognizer;
-    private final Map<String, Integer> captions;
-    private ProgressDialog dialog;
-
-    public PocketSphinxActivity() {
-        captions = new HashMap<String, Integer>();
-        captions.put(KWS_SEARCH_NAME, R.string.kws_caption);
-        captions.put(MENU_SEARCH, R.string.menu_caption);
-        captions.put(DIGITS_SEARCH, R.string.digits_caption);
-        captions.put(FORECAST_SEARCH, R.string.forecast_caption);
-    }
+    private HashMap<String, Integer> captions;
 
     @Override
     public void onCreate(Bundle state) {
         super.onCreate(state);
+
+        // Prepare the data for UI
+        captions = new HashMap<String, Integer>();
+        captions.put(KWS_SEARCH, R.string.kws_caption);
+        captions.put(MENU_SEARCH, R.string.menu_caption);
+        captions.put(DIGITS_SEARCH, R.string.digits_caption);
+        captions.put(FORECAST_SEARCH, R.string.forecast_caption);
         setContentView(R.layout.main);
-        new AssetsTask(this, this).execute();
+        ((TextView) findViewById(R.id.caption_text))
+                .setText("Preparing the recognizer");
+
+        // Recognizer initialization is a time-consuming and it involves IO,
+        // so we execute it in async task
+
+        new AsyncTask<Void, Void, Exception>() {
+            @Override
+            protected Exception doInBackground(Void... params) {
+                try {
+                    Assets assets = new Assets(PocketSphinxActivity.this);
+                    File assetDir = assets.syncAssets();
+                    setupRecognizer(assetDir);
+                } catch (IOException e) {
+                    return e;
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Exception result) {
+                if (result != null) {
+                    ((TextView) findViewById(R.id.caption_text))
+                            .setText("Failed to init recognizer " + result);
+                } else {
+                    switchSearch(KWS_SEARCH);
+                }
+            }
+        }.execute();
     }
 
     @Override
@@ -60,17 +118,9 @@ public class PocketSphinxActivity extends Activity implements
     public void onResult(Hypothesis hypothesis) {
         ((TextView) findViewById(R.id.result_text)).setText("");
         if (hypothesis != null) {
-    	    String text = hypothesis.getHypstr();
-    	    makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
-	}
-    }
-
-    private void switchSearch(String searchName) {
-        recognizer.stop();
-        recognizer.startListening(searchName);
-
-        String caption = getResources().getString(captions.get(searchName));
-        ((TextView) findViewById(R.id.caption_text)).setText(caption);
+            String text = hypothesis.getHypstr();
+            makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
@@ -81,26 +131,27 @@ public class PocketSphinxActivity extends Activity implements
     public void onEndOfSpeech() {
         if (DIGITS_SEARCH.equals(recognizer.getSearchName())
                 || FORECAST_SEARCH.equals(recognizer.getSearchName()))
-            switchSearch(KWS_SEARCH_NAME);
+            switchSearch(KWS_SEARCH);
     }
 
-    @Override
-    public void onTaskCancelled() {
+    private void switchSearch(String searchName) {
+        recognizer.stop();
+        recognizer.startListening(searchName);
+        String caption = getResources().getString(captions.get(searchName));
+        ((TextView) findViewById(R.id.caption_text)).setText(caption);
     }
 
-    @Override
-    public void onTaskComplete(File assetsDir) {
+    private void setupRecognizer(File assetsDir) {
         File modelsDir = new File(assetsDir, "models");
         recognizer = defaultSetup()
                 .setAcousticModel(new File(modelsDir, "hmm/en-us-semi"))
                 .setDictionary(new File(modelsDir, "lm/cmu07a.dic"))
-                .setRawLogDir(assetsDir)
-                .setKeywordThreshold(1e-5f)
+                .setRawLogDir(assetsDir).setKeywordThreshold(1e-20f)
                 .getRecognizer();
-
         recognizer.addListener(this);
+
         // Create keyword-activation search.
-        recognizer.addKeyphraseSearch(KWS_SEARCH_NAME, KEYPHRASE);
+        recognizer.addKeyphraseSearch(KWS_SEARCH, KEYPHRASE);
         // Create grammar-based searches.
         File menuGrammar = new File(modelsDir, "grammar/menu.gram");
         recognizer.addGrammarSearch(MENU_SEARCH, menuGrammar);
@@ -109,29 +160,5 @@ public class PocketSphinxActivity extends Activity implements
         // Create language model search.
         File languageModel = new File(modelsDir, "lm/weather.dmp");
         recognizer.addNgramSearch(FORECAST_SEARCH, languageModel);
-
-        switchSearch(KWS_SEARCH_NAME);
-        dialog.dismiss();
-    }
-
-    @Override
-    public void onTaskError(Throwable e) {
-        if (dialog.isShowing())
-            dialog.dismiss();
-        ((TextView) findViewById(R.id.caption_text)).setText(e.getMessage());
-    }
-
-    @Override
-    public void onTaskProgress(File file) {
-        dialog.incrementProgressBy(1);
-    }
-
-    @Override
-    public void onTaskStart(int size) {
-        dialog = new ProgressDialog(this);
-        dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-        dialog.setTitle("Copying model files...");
-        dialog.setMax(size);
-        dialog.show();
     }
 }
